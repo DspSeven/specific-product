09b1a37f5c1195dc1cd25713bb7cc3c5610f31ab
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-01-24 15:54:42 +0530

    Update index.js

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,1 +1,30 @@
 // Write your code here
+// Write your code here
+const SimilarProductItem = props => {
+  const {productDetails} = props
+  const {
+    availability,
+    brand,
+    description,
+    imageUrl,
+    price,
+    rating,
+    similarProducts,
+    style,
+    title,
+    totalReviews,
+  } = productDetails
+  return (
+    <li>
+      <img src={imageUrl} alt={`similar product ${title}`} />
+      <h1>{title}</h1>
+      <p>Rs {price}</p>
+      <p>{rating}</p>
+      <p>{totalReviews} Reviews</p>
+      <p>{description}</p>
+      <p>Available: {availability}</p>
+      <p>Brand: {brand}</p>
+    </li>
+  )
+}
+export default SimilarProductItem
